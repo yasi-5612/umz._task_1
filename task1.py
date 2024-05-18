86b488c004664f202e3612c2f5072ab8177eb49d
yasi-5612 <162043937+yasi-5612@users.noreply.github.com>
2024-05-18 22:37:43 +0330

    Delete task1.py

--- task1.py
+++
@@ -1,8 +0,0 @@
-user_input = input("Please enter your entry: ")
-
-if user_input[0] in '-0123456789':
-    print("The input is an integer.")
-elif ',' in user_input:
-    print("The input is a list.")
-else:
-    print("The input is a string.")
